ea60b55ea417c59ba78316db04aa613f3a02b478
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-08-26 12:09:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let ans=0;
+	while(arr.length>1)
+	{
+		arr.sort((a,b)=>a-b);
+		let f=arr.shift();
+		let s=arr.shift();
+
+		let temp=Number(f+s);
+		ans+=temp;
+		arr.push(temp);
+	}
+	return ans;
 }
 
 module.exports=mincost;
